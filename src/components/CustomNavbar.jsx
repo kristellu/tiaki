eff486d0d8a9cc65352da5d89465666d4107dbe3
Kristell <kristellu@uninorte.edu.co>
2018-11-05 10:47:25 -0500

    about+news

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './CustomNavbar.css'; //para llamar el diseño css a este comp
 
 export default class CustomNavbar extends Component{
     render(){
@@ -14,13 +15,12 @@ export default class CustomNavbar extends Component{
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav pullRight>
-                        <NavItem eventKey={1} componentClass={Link} to="/">
-                        Home
+                        <NavItem eventKey={1} componentClass={Link} href="/" to="/">
                         </NavItem>
-                        <NavItem eventKey={2} componentClass={Link} to="/about">
+                        <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
                         About
                         </NavItem>
-                        <NavItem eventKey={3} componentClass={Link} to="/news">
+                        <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
                         News
                         </NavItem>
                     </Nav>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,10 +10,27 @@ export default class Home extends Component{
                 <Jumbotron>
                    <h2>COMPRAR BARATO ES SIMPLE</h2>
                    <p>This is our page, React-Router & Reat-bootstrap</p>
+                    <Link to= "/about">
+                        <Button bsStyle="danger">About us</Button>
+                    </Link>
                 </Jumbotron>
-                <Link to= "/about">
-                    <Button bsStyle="primary">About us</Button>
-                </Link>
+                <Row className="show-grid text-center">
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/buyDigital.jpg" circle className="profile-pic"/>
+                        <h3>Los mejores precios</h3>
+                        <p>That's a croocked tree.</p>
+                    </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/carrosCompra.jpg" circle className="profile-pic"/>
+                        <h3>Compra facil</h3>
+                        <p>That's a croocked tree.</p>
+                    </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/datafono.jpg" square className="profile-pic"/>
+                        <h3>Ahorra</h3>
+                        <p>That's a croocked tree.</p>
+                    </Col>
+                </Row>
             </Grid>
         )
     }
