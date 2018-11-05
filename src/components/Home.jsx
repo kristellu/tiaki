bbbf2b1c6b9ba9f9c2f6d3413f629297be213669
Kristell <kristellu@uninorte.edu.co>
2018-11-05 01:01:06 -0500

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React,   {Component} from 'react'
+import{ Link } from 'react-router-dom';
+import {Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import './Home.css';
+
+export default class Home extends Component{
+    render(){
+        return(
+            <Grid>
+                <Jumbotron>
+                   <h2>WELCOME TO TIAKI</h2>
+                   <p>This is our page, React-Router & Reat-bootstrap</p>
+                </Jumbotron>
+                <Link to= "/about">
+                    <Button bsStyle="primary">About</Button>
+                </Link>
+            </Grid>
+        )
+    }
+}
